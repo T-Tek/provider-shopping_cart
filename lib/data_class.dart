dca35c98cebf2e28183269564cb6ea2f780e664a
T-Tek <toppyech@gmail.com>
2022-04-12 00:04:31 +0800

    On branch main

---
+++ lib/data_class.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class DataClass extends ChangeNotifier {
+  int _x = 0;
+  int get x => _x;
+  void incrementX() {
+    _x++;
+    notifyListeners();
+  }
+
+  void decrementX() {
+    _x--;
+    notifyListeners();
+  }
+}
